ac80d901560ababd614f2cd69a44a37eb660cafd
Kara Anderson <kanderson8585@gmail.com>
2023-06-01 15:04:46 -0600

    Write tests for MVP

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,5 +13,48 @@ describe('template spec', () => {
       .get('.articles > :nth-child(1)').contains('Russia-Ukraine')
       .get('.articles > :nth-child(2)').contains('Memorial Day')
       .get('.articles > :nth-child(3)').contains('Senate impeachment')
+      .url().should('equal', 'http://localhost:3000/')
+  });
+
+  it('should allow users to click the read more and show a detailed article view', () => {
+    cy.get(':nth-child(2) > :nth-child(3) > .published > a > .read-more').click()
+      .url().should('equal', 'http://localhost:3000/article')
+      .get('.article-details').should('exist')
+      .get('.title').should('have.text', '9 Hospitalized After Memorial Day Shooting at Hollywood Beach Broadwalk - NBC 6 South Florida')
+  });
+
+  it('should allow the user to navigate back to the main page', () => {
+    cy.url().should('equal', 'http://localhost:3000/')
+      .get(':nth-child(2) > :nth-child(3) > .published > a > .read-more').click()
+      .url().should('equal', 'http://localhost:3000/article')
+      .get('.header').click()
+      .url().should('equal', 'http://localhost:3000/')
+  });
+
+  it('should allow the user to search for an article by keyword', () => {
+    cy.get('input').type('Senate')
+      .get('.article-preview').should('have.length', 1)
+      .get('.articles > :nth-child(1)').contains('Senate impeachment')
+  });
+
+  it('should navigate back to homepage when the searchbar is empty', () => {
+    cy.get('input').type('Senate')
+      .get('.article-preview').should('have.length', 1)
+      .get('input').clear()
+      .get('.article-preview').should('have.length', 3)
+  });
+
+  it('should show the user an error message if there is no search results', () => {
+    cy.get('input').type('nonsense')
+      .get('p').should('have.text', 'No results for that search term.')
+      .get('.article-preview').should('not.exist')
+  });
+
+  it('should hide the search bar when not on the homepage', () => {
+    cy.get('input').should('exist')
+      .get(':nth-child(2) > :nth-child(3) > .published > a > .read-more').click()
+      .url().should('equal', 'http://localhost:3000/article')
+      .get('.olaf').should('exist')
+      .get('input').should('not.exist')
   });
 })
