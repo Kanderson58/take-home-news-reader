b1f79a95d9414d080f20df0cff357f9449a19131
Kara Anderson <kanderson8585@gmail.com>
2023-06-02 09:47:35 -0600

    Handle accessibility concerns

--- src/components/ArticlePreview/ArticlePreview.js
+++ src/components/ArticlePreview/ArticlePreview.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ArticlePreview = ({article, setSelectedArticle}) => {   
   return (
-    <div className='article-preview'>
+    <div className='article-preview' tabIndex='0'>
       <h2>{article.title}</h2>
       {article.description && <p className='description'>{article.description}</p>}
       <div>
@@ -12,7 +12,7 @@ const ArticlePreview = ({article, setSelectedArticle}) => {
         </div>
         <div className='published'>
           <p>Published: <span className='date'>{article.publishedAt.substring(0, 10)}</span></p>
-          <Link to='/article'><button className='read-more' onClick={() => setSelectedArticle(article)}>Read More</button></Link>
+          <Link to='/article' tabIndex='-1'><button className='read-more' onClick={() => setSelectedArticle(article)}>Read More</button></Link>
         </div>
       </div>
     </div>
